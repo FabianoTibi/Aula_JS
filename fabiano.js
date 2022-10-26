4d36e35431a14990ea8e0d21190714e49bf5035d
fabiano <fariadevs2022@gmail.com>
2022-10-26 17:08:33 -0300

    primeiros testes

---
+++ fabiano.js
@@ -0,0 +1,11 @@
+let parcela1 = 15
+let parcela2 = 20
+let resultado = 0
+var parcela3 = 150
+var parcela4 = 200
+var resultado2 = 0
+let meuNome = "Fabiano"
+var meuNome2 = "Fabiano Augusto de Faria"
+let parcelaJunta = parcela1
+
+
